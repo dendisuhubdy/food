9a7d3c176fcc401e3ce5b9aad8ea5640521cbaa4
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2017-09-28 21:49:07 -0400

    adding skeleton

---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+#include <boost/program_options.hpp>
+ 
+#include <iostream> 
+#include <string> 
+ 
+namespace 
+{ 
+  const size_t ERROR_IN_COMMAND_LINE = 1; 
+  const size_t SUCCESS = 0; 
+  const size_t ERROR_UNHANDLED_EXCEPTION = 2; 
+ 
+} // namespace 
+ 
+int main(int argc, char** argv) 
+{ 
+  try 
+  { 
+    /** Define and parse the program options 
+     */ 
+    namespace po = boost::program_options; 
+    po::options_description desc("Options"); 
+    desc.add_options() 
+      ("--food-type", "Food type") 
+      ("--gps", "Latitude and Longitude of Location of Food Delivery") 
+      ("--cc", "Payment Credit Card");
+ 
+    po::variables_map vm; 
+    try 
+    { 
+      po::store(po::parse_command_line(argc, argv, desc),  
+                vm); // can throw 
+ 
+      /** --help option 
+       */ 
+      if ( vm.count("help")  ) 
+      { 
+        std::cout << "Basic Command Line Parameter App" << std::endl 
+                  << desc << std::endl; 
+        return SUCCESS; 
+      } 
+ 
+      po::notify(vm); // throws on error, so do after help in case 
+                      // there are any problems 
+    } 
+    catch(po::error& e) 
+    { 
+      std::cerr << "ERROR: " << e.what() << std::endl << std::endl; 
+      std::cerr << desc << std::endl; 
+      return ERROR_IN_COMMAND_LINE; 
+    } 
+ 
+    // application code here // 
+ 
+  } 
+  catch(std::exception& e) 
+  { 
+    std::cerr << "Unhandled Exception reached the top of main: " 
+              << e.what() << ", application will now exit" << std::endl; 
+    return ERROR_UNHANDLED_EXCEPTION; 
+ 
+  } 
+ 
+  return SUCCESS; 
+ 
+} // main 
